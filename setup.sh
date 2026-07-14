4cdef2d8ecce566d590b2982d2d23a33fbe84f43
Owen Qian <owen@triumpharcade.com>
2026-07-14 16:20:14 -0700

    add cursor setup settings

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@
 # after installing or updating yadr, point the .vimrc simlink to the .vimrc in this directory
 # ln -s vimfiles/.vimrc .vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+VIMFILES="${VIMFILES:-$HOME/vimfiles}"
+CURSOR_USER_DIR="$HOME/Library/Application Support/Cursor/User"
+
+mkdir -p "$CURSOR_USER_DIR"
+ln -sfn "$VIMFILES/cursor/settings.json" "$CURSOR_USER_DIR/settings.json"
